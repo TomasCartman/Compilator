642489a8df3c6686a55282f658f0916c09f7b8d3
Tomas Cartman <marco12y@outlook.com>
2021-03-01 11:27:29 -0300

    add class to test AutomatonRelationalOperators

---
+++ tests/AutomatonRelationalOperatorsTest.kt
@@ -0,0 +1,46 @@
+package tests
+
+import lexicalAnalyzer.AutomatonRelationalOperators
+import org.junit.jupiter.api.Assertions.*
+
+internal class AutomatonRelationalOperatorsTest {
+    private lateinit var automaton: AutomatonRelationalOperators
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+        automaton = AutomatonRelationalOperators()
+    }
+
+    @org.junit.jupiter.api.Test
+    fun putNewString() {
+        // Valid
+        assertEquals(true, automaton.putNewString(">"))
+        assertEquals(true, automaton.putNewString("!="))
+        assertEquals(true, automaton.putNewString("=="))
+        assertEquals(true, automaton.putNewString("<"))
+        assertEquals(true, automaton.putNewString("="))
+        assertEquals(true, automaton.putNewString("<="))
+        assertEquals(true, automaton.putNewString(">="))
+
+        // Invalid
+        assertEquals(false, automaton.putNewString("a"))
+        assertEquals(false, automaton.putNewString(" "))
+        assertEquals(false, automaton.putNewString(">>"))
+        assertEquals(false, automaton.putNewString("abc"))
+        assertEquals(false, automaton.putNewString("(>"))
+        assertEquals(false, automaton.putNewString("==="))
+        assertEquals(false, automaton.putNewString("  "))
+        assertEquals(false, automaton.putNewString("."))
+        assertEquals(false, automaton.putNewString("\n"))
+        assertEquals(false, automaton.putNewString("\\"))
+        assertEquals(false, automaton.putNewString("<!"))
+        assertEquals(false, automaton.putNewString("=!"))
+        assertEquals(false, automaton.putNewString("<>"))
+        assertEquals(false, automaton.putNewString("><"))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun generateToken() {
+
+    }
+}
